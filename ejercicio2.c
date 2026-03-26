b0203363664572f7f26d515cf831f2bea400b417
Nicolás Cuello <jaimecuellosuarez@gmail.com>
2026-03-26 01:06:20 -0500

    New methods for second point added, nearly finishing it, at least.

--- ejercicio2.c
+++ ejercicio2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 double* generarVector(int n, unsigned int semilla) {
     double* vector = malloc(n * sizeof(double));
@@ -21,7 +22,11 @@ double calcularMin(const double *vector, int n) {
     int i = 0;
     double min = *(vector + i);
 
-    for (i = 0; i < n; i++) {
+    if (n <= 0 || vector == NULL) {
+        return 0;
+    }
+
+    for (i = 1; i < n; i++) {
         if (*(vector + i) < min) {
             min = *(vector + i);
         }
@@ -34,7 +39,11 @@ double calcularMax(const double *vector, int n) {
     int i = 0;
     double max = *(vector + i);
 
-    for (i = 0; i < n; i++) {
+    if (n <= 0 || vector == NULL) {
+        return 0;
+    }
+
+    for (i = 1; i < n; i++) {
         if (*(vector + i) > max) {
             max = *(vector + i);
         }
@@ -69,9 +78,56 @@ double calcularMedia(const double *vector, int n) {
 
 double calcularVarianza(const double *vector, int n, double media) {
     int i = 0;
-    double suma = 0.0;
+    double suma_de_cuadrados = 0.0;
+    double varianza;
 
+    for (i = 0; i < n; i++) {
+        suma_de_cuadrados += (*(vector + i) - media) * (*(vector + i) - media);
+    }
 
+    varianza = suma_de_cuadrados / n;
+    return varianza;
+}
+
+double calcularDesviacion(double varianza) {
+    double desviacion = sqrt(varianza);
+    return desviacion;
+}
+
+void normalizarMinMax(double *vector, int n, double min, double max) {
+    // Validación inicial; si son iguales, puede existir división entre 0.
+    if (min == max) {
+        return;
+    }
+
+    int i = 0;
+    double *ptr = vector;
+
+    // Uso de punteros para el recorrido, actualización del objeto
+    // con la fórmula descrita.
+    for (i = 0; i < n; i++, ptr++) {
+        *ptr = (*ptr - min) / (max - min);
+    }
+}
+
+void imprimirVector(const double *vector, int n, int por_linea) {
+    int i = 0;
+    int counter = 0;
+    double *ptr = vector;
+
+    for (i = 0; i < n; i ++, ptr++) {
+        printf("%8.4f", *ptr);
+        counter++;
+
+        if (counter == por_linea) {
+            printf("\n");
+            counter = 0;
+        }
+    }
+}
+
+void reporteEstadisticas(const double *vector, int n, const char *titulo) {
+    printf("El mínimo es %d", calcularMin(vector, n));
 
 }
 
